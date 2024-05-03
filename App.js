d40ce0dcff0494dd76f7909f07c320760518d438
Dinhtraniii <tranvandinh2502@gmail.com>
2024-05-03 12:21:29 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Provider } from 'react-redux'; 
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+import Store from './store';
+
+import DrawerNavigator from './4/routes';
+
+const App = () =>{
+  return(
+    
+      <Provider store={Store}>
+        <DrawerNavigator/>
+      </Provider>
+  )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
